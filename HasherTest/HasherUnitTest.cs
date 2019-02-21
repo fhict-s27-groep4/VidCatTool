5ddd3bc055ac1654e7cde98132d3bace3b620bc4
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-02-21 16:04:36 +0100

    unittest en accountcontroller

---
+++ HasherTest/HasherUnitTest.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using VidCat_Tool;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HasherTest
+{
+    public class HasherUnitTest
+    {
+        private readonly VidCat_Tool.Controllers.AccountController vidcat;
+
+        public HasherUnitTest()
+        {
+            vidcat = new VidCat_Tool.Controllers.AccountController();
+        }
+
+        //[Fact]
+        //public void Hasher_Met_Salt_Twee_Keer_Hetzelfde()
+        //{
+        //    string password = "henk";
+        //    vidcat.HashWithSalt(password);
+        //    string first = vidcat.hashtotal;
+        //    string key = vidcat.key;
+        //    var result = vidcat.checkpassword(password, key);
+        //    Assert.Equal(first, result);
+        //}
+
+        //[Fact]
+        //public void Twee_Keer_Niet_Dezelfde_Key()
+        //{
+        //    string een = vidcat.GenerateRandomCryptographicKey(64);
+        //    string twee = vidcat.GenerateRandomCryptographicKey(64);
+        //    Assert.NotEqual(een, twee);
+        //}
+    }
+}
---
+++ VidCat_Tool/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VidCat_Tool.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        #region Fields
+
+        public string key = "";
+        public string hashtotal;
+        private string result;
+
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Hasher en Salt
+
+        public void HashWithSalt(string password) // nieuw wachtwoord hashen
+        {
+            key = GenerateRandomCryptographicKey(64); // maak een random salt aan
+            string total = password + key; // voeg salt en password samen
+            byte[] textData = System.Text.Encoding.UTF8.GetBytes(total); // zet het om naar bytes
+            byte[] hash = SHA256.Create().ComputeHash(textData); // creer de hash
+            hashtotal = BitConverter.ToString(hash); // zet bytes om naar string
+        }
+        public string checkpassword(string password, string salt) // ingevulde wachtwoord checken
+        {
+            string total = password + salt; // samenvoegen wachtwoord en salt ( de uit de list komt met dezelfde index als de gebruiker)
+            byte[] textData = System.Text.Encoding.UTF8.GetBytes(total); // omzetten naar bytes
+            byte[] hash = SHA256.Create().ComputeHash(textData); // hashen van salt en wachtwoord
+            return result = BitConverter.ToString(hash); // omzetten van bytes naar string en terugsturen om te checken
+        }
+        public string GenerateRandomCryptographicKey(int keyLength)
+        {
+            return Convert.ToBase64String(GenerateRandomCryptographicBytes(keyLength)); //lengte van key
+        }
+        public byte[] GenerateRandomCryptographicBytes(int keyLength)
+        {
+            RNGCryptoServiceProvider rngCryptoServiceProvider = new RNGCryptoServiceProvider(); // aanmaken van random bytes
+            byte[] randomBytes = new byte[keyLength];
+            rngCryptoServiceProvider.GetBytes(randomBytes);
+            return randomBytes;
+        }
+
+        #endregion
+    }
+}
