ec8aa47bc22605c763da022cc9f447bc3063c01d
BlackMania <BlackMania@users.noreply.github.com>
2019-02-26 09:11:46 +0100

    Changed some of the css and some names of the files.

--- VidCat_Tool/ViewModels/LoginViewModel.cs
+++ VidCat_Tool/ViewModels/LoginViewModel.cs
@@ -7,7 +7,7 @@ namespace VidCat_Tool.ViewModels
         [Required, Display(Name ="Username", Prompt = "Username")]
         public string UserName { get; set; }
 
-        [Required, Display(Name = "Password", Prompt = "Password")]
+        [Required, Display(Name = "Password", Prompt = "Password"), DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
