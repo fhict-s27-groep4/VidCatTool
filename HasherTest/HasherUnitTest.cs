d89d5f5104ed695af32847e74e2b27447f7d0f8c
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-02-26 12:16:34 +0100

    changed unit test to net core 2.1, updated aspnetcore reference

--- HasherTest/HasherUnitTest.cs
+++ HasherTest/HasherUnitTest.cs
@@ -14,23 +14,23 @@ namespace HasherTest
             vidcat = new VidCat_Tool.Controllers.AccountController();
         }
 
-        //[Fact]
-        //public void Hasher_Met_Salt_Twee_Keer_Hetzelfde()
-        //{
-        //    string password = "henk";
-        //    vidcat.HashWithSalt(password);
-        //    string first = vidcat.hashtotal;
-        //    string key = vidcat.key;
-        //    var result = vidcat.checkpassword(password, key);
-        //    Assert.Equal(first, result);
-        //}
+        [Fact]
+        public void Hasher_Met_Salt_Twee_Keer_Hetzelfde()
+        {
+            string password = "henk";
+            vidcat.HashWithSalt(password);
+            string first = vidcat.hashtotal;
+            string key = vidcat.key;
+            var result = vidcat.checkpassword(password, key);
+            Assert.Equal(first, result);
+        }
 
-        //[Fact]
-        //public void Twee_Keer_Niet_Dezelfde_Key()
-        //{
-        //    string een = vidcat.GenerateRandomCryptographicKey(64);
-        //    string twee = vidcat.GenerateRandomCryptographicKey(64);
-        //    Assert.NotEqual(een, twee);
-        //}
+        [Fact]
+        public void Twee_Keer_Niet_Dezelfde_Key()
+        {
+            string een = vidcat.GenerateRandomCryptographicKey(64);
+            string twee = vidcat.GenerateRandomCryptographicKey(64);
+            Assert.NotEqual(een, twee);
+        }
     }
 }
