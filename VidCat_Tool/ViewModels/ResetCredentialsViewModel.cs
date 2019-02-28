f160267c4a2563f792e531b328043673291d43a8
Martin Peeters <406336@student.fontys.nl>
2019-02-28 14:41:05 +0100

    Added ResetCredentials. CSS is in HTML-Style, has to be split to a different file. Added page to AccountController too.

---
+++ VidCat_Tool/ViewModels/ResetCredentialsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VidCat_Tool.ViewModels
+{
+    public class ResetCredentialsViewModel
+    {
+        [Required, Display(Name = "EMail", Prompt = "EMail"), DataType(DataType.EmailAddress)]
+        public string EMail { get; set; }
+
+        [Required, Display(Name = "ResetType", Prompt = "ResetType")]
+        public string ResetType { get; set; }
+    }
+}
