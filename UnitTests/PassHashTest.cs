de4b3bac296b4f5bdacae4addb38c80d136eda66
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-03-12 10:48:11 +0100

    Rename, Gefixt unit Test

---
+++ UnitTests/PassHashTest.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using VidCat_Tool.Controllers;
+using BusinessLogicLibrary.PasswordHasher.BusinessLogicLibrary.PassHasher;
+using System.Security.Authentication;
+using NuGet.Frameworks;
+
+
+namespace UnitTests
+{
+    public class HasherUnitTest
+    {
+        private readonly PasswordHasher PassHash;
+
+        public HasherUnitTest()
+        {
+            PassHash = new PasswordHasher();
+        }
+
+        [Fact]
+        public void Hasher_Met_Salt_Twee_Keer_Hetzelfde()
+        {
+            string password = "henk";
+            PassHash.HashWithSalt(password);
+            string first = PassHash.hashtotal;
+            string key = PassHash.key;
+            var result = PassHash.checkpassword(password, key);
+            Assert.Equal(first, result);
+        }
+
+        [Fact]
+        public void Twee_Keer_Niet_Dezelfde_Key()
+        {
+            string een = PassHash.GenerateRandomCryptographicKey(64);
+            string twee = PassHash.GenerateRandomCryptographicKey(64);
+            Assert.NotEqual(een, twee);
+        }
+    }
+}
