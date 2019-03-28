ebf8f59c51345a212c7f9c46354100cd021d246d
Vincent Muijtjens <411946@student.fontys.nl>
2019-03-28 22:34:17 +0100

    Added viewmodel for the review page.

--- VidCat_Tool/ViewModels/ReviewViewModel.cs
+++ VidCat_Tool/ViewModels/ReviewViewModel.cs
@@ -2,21 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace VidCat_Tool.ViewModels
 {
     public class ReviewViewModel
     {
-        [Required]
-        public int VideoID { get; set; }
-        [Required]
-        public int IABID { get; set; }
-        [Required]
-        public int Pleasure { get; set; }
-        [Required]
-        public int Arousal { get; set; }
-        [Required]
-        public int Dominance { get; set; }
+        public ReviewViewModelGet ReviewGetInfo { get; set; }
+        public ReviewViewModelPost ReviewPostInfo { get; set; }
     }
 }
---
+++ VidCat_Tool/ViewModels/ReviewViewModelPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VidCat_Tool.ViewModels
+{
+    public class ReviewViewModelPost
+    {
+        [Required]
+        public int VideoID { get; set; }
+        [Required]
+        public int IABID { get; set; }
+        [Required]
+        public int Pleasure { get; set; }
+        [Required]
+        public int Arrousal { get; set; }
+        [Required]
+        public int Dominance { get; set; }
+    }
+}
