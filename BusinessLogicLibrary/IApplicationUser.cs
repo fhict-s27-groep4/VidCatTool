b79aabe7073fdeff90a9316b17763fc7507b601f
BlackMania <BlackMania@users.noreply.github.com>
2019-04-04 10:46:44 +0200

    Added ApplicationUser Classes

---
+++ BusinessLogicLibrary/IApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLibrary
+{
+    public interface IApplicationUser
+    {
+        void UpdateContactInformation();
+
+        bool UpdatePassword();
+
+
+    }
+}
