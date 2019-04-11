1ebb8daedb42a31061195467c3e70cfed1274249
Niek Sleddens <I413747@fhict.nl>
2019-04-11 15:18:43 +0200

    Algoritm ratings

---
+++ BusinessLogicLibrary/AlgoritmRatings/DivergentRatings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLibrary.AlgoritmRatings
+{
+    public class DivergentRatings : EventArgs
+    {
+        private IEnumerable<IRating> ratings;
+
+        public DivergentRatings(IEnumerable<IRating> _ratings)
+        {
+            ratings = _ratings;
+        }
+
+        public IEnumerable<IRating> Ratings
+        {
+            get { return ratings; }
+        }
+    }
+}
