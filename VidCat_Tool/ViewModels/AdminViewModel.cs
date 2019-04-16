1e76e814e131374ecbc081187de928bfdc6a7fdd
Martin Peeters <406336@student.fontys.nl>
2019-04-16 10:39:51 +0200

    commit

---
+++ VidCat_Tool/ViewModels/AdminViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VidCat_Tool.ViewModels
+{
+    public class AdminViewModel
+    {
+        public List<User> SelectedUsers { get; set; }
+
+    }
+}
