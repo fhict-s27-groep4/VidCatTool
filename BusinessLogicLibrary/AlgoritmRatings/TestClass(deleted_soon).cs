edbda4c8c6179034136a43faae2121bd5690ae27
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-04-18 14:52:24 +0200

    Unittest ratingalgorithme added

---
+++ BusinessLogicLibrary/AlgoritmRatings/TestClass(deleted_soon).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLibrary.AlgoritmRatings
+{
+    class TestClass_deleted_soon_
+    {
+        public TestClass_deleted_soon_()
+        {
+            List<IRating> test = new List<IRating>();
+            //(1, 2, false, 4, 3, 4, false, test)
+            Rating rating1 = new Rating();
+            rating1.setAll(5, 5, 4, 4, 4);
+            Rating rating2 = new Rating();
+            rating2.setAll(5, 5, 4, 4, 4);
+            Rating rating3 = new Rating();
+            rating3.setAll(5, 5, 4, 4, 4);
+            Rating rating4 = new Rating();
+            rating4.setAll(5, 5, 1, 1, 1);
+            test.Add(rating1);
+            test.Add(rating2);
+            test.Add(rating3);
+            test.Add(rating4);
+            RatingAlgoritm ratalgo = new RatingAlgoritm();
+            ratalgo.FindDivergents(test);
+            
+        }
+        
+    }
+}
