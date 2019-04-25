9f421a73d0b99cf32420783393ae15720677e4ad
BlackMania <BlackMania@users.noreply.github.com>
2019-04-25 09:55:30 +0200

    AdminCheck added

---
+++ VidCat_Tool/Controllers/AdminCheck.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogicLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VidCat_Tool.Controllers
+{
+    public class AdminCheck : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (string.IsNullOrEmpty(filterContext.HttpContext.Session.GetString("Username")))
+            {
+                var test = filterContext.HttpContext.Session.GetString("Username");
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary {
+                                { "Controller", "Home" },
+                                { "Action", "Dashboard" }
+                                });
+            }
+        }
+    }
+}
