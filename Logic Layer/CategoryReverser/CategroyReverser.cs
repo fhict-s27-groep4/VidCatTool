ce4910e7eaf3ee093e8fa561112ef8ccb20a466f
Niek Sleddens <I413747@fhict.nl>
2019-05-07 12:38:23 +0200

    categoryreverser

--- Logic Layer/CategoryReverser/CategroyReverser.cs
+++ Logic Layer/CategoryReverser/CategroyReverser.cs
@@ -1,23 +1,29 @@
 ﻿using Model_Layer.Interface;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Logic_Layer.CategoryReverser
 {
     public class CategroyReverser
     {
-        IEnumerable<IRating> ratings;
-        public CategroyReverser(IEnumerable<IRating> _ratings)
+        IEnumerable<ICategory> categories;
+        public CategroyReverser(IEnumerable<ICategory> _categories)
         {
-            ratings = _ratings;
+            categories = _categories;
         }
 
         public IObjectPair<int, int> GetParentTiers(ICategory _category)
         {
             IObjectPair<int, int> parentTiers = new ObjectPair<int, int>();
-            parentTiers.Object1 = _category.ParentID;
+            parentTiers.Object1 = (int)_category.ParentID;
             parentTiers.Object1 = _category.UniqueID;
             ICategory current = _category;
-            while(current.ParentID)
+            while(current.ParentID != null)
+            {
+                current = categories.Where(x => x.UniqueID == parentTiers.Object1).First();
+                parentTiers.Object1 = (int)current.ParentID;
+                parentTiers.Object1 = current.UniqueID;
+            }
             return parentTiers;
         }
     }
