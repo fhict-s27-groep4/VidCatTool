9840ff12c7301eb25681248cfc10cd54c25002c9
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-05-07 10:33:09 +0200

    Unittest For Get/Set Classes done

---
+++ UnitTests/SmallTests/VideoClassTest.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLogicLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.SmallTests
+{
+    public class VideoClassTest
+    {
+        Video vid;
+        public VideoClassTest()
+        {
+            vid = new Video();   
+        }
+
+        [Fact]
+        public void CheckGetSetVideo()
+        {
+            //arrange
+            vid.UrlIdentity = "Tester123";
+            vid.VideoID = 12;
+            vid.VideoURL = "Whoop.com";
+
+            //assert
+            Assert.Equal("Tester123", vid.UrlIdentity);
+            Assert.Equal(12, vid.VideoID);
+            Assert.Equal("Whoop.com", vid.VideoURL);
+        }
+
+        [Fact]
+        public void CheckGetSetFalseVideo()
+        {
+            vid.UrlIdentity = "Tester123";
+            vid.VideoID = 12;
+            vid.VideoURL = "Whoop.com";
+
+            Assert.NotEqual("Tester124", vid.UrlIdentity);
+            Assert.NotEqual(15, vid.VideoID);
+            Assert.NotEqual("Whoops.com", vid.VideoURL);
+        }
+    }
+}
