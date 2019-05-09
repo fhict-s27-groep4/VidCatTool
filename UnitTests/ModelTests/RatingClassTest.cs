d74fbe9adb125686c9bde803874865052c6d14fb
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-05-09 09:23:17 +0200

    Opzet Ratingmodel test done maar moet uitstappen

---
+++ UnitTests/ModelTests/RatingClassTest.cs
@@ -0,0 +1,35 @@
+﻿using Model_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.ModelTests
+{
+    public class RatingClassTest
+    {
+        private Rating rating;
+        public RatingClassTest()
+        {
+            rating = new Rating();
+        }
+
+        [Fact]
+        public void CheckGetSetRating()
+        {
+
+        }
+
+        [Fact]
+        public void CheckGetSetNotEqualRating()
+        {
+
+        }
+        
+        [Fact]
+        public void CheckGetSetNotEmptyRating()
+        {
+
+        }
+    }
+}
