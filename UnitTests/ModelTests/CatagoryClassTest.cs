bcd5128adbaa1dffb52576d1da0ab34b46035455
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-05-09 12:36:39 +0200

    Unit Test Catoryreverser done and working, 100% COVERAGE MODEL LAYER

---
+++ UnitTests/ModelTests/CatagoryClassTest.cs
@@ -0,0 +1,66 @@
+﻿using Model_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.ModelTests
+{
+    public class CatagoryClassTest
+    {
+        Category catagory;
+        public CatagoryClassTest()
+        {
+            catagory = new Category();
+        }
+        [Fact]
+        public void CheckGetSetCategory()
+        {
+            //arange
+            catagory.UniqueID = 1;
+            catagory.ParentID = 5;
+            catagory.Name = "Auto";
+
+            //assert
+            Assert.Equal(1, catagory.UniqueID);
+            Assert.Equal(5, catagory.ParentID);
+            Assert.Equal("Auto", catagory.Name);
+        }
+        [Fact]
+        public void CheckGetSetNotEqualCategory()
+        {
+            //arange
+            catagory.UniqueID = 1;
+            catagory.ParentID = 5;
+            catagory.Name = "Auto";
+
+            //assert
+            Assert.NotEqual(2, catagory.UniqueID);
+            Assert.NotEqual(7, catagory.ParentID);
+            Assert.NotEqual("Autos", catagory.Name);
+        }
+        [Fact]
+        public void CheckRatingCategory()
+        {
+            //arrange
+            List<Rating> Ratinglist = new List<Rating>();
+            Rating rating1 = new Rating();
+            rating1.ArrousalIndex = 1;
+            rating1.CategoryID = 4;
+            rating1.DominanceIndex = 4;
+            rating1.IsIABDivergent = false;
+            rating1.IsPADDivergent = true;
+            rating1.PleasureIndex = 4;
+            rating1.RatingID = 5;
+            rating1.UserID = "15";
+            rating1.VideoIdentity = "ABCDEFG";
+
+            //act
+            Ratinglist.Add(rating1);
+            catagory.Ratings = Ratinglist;
+
+            //assert
+            Assert.NotEmpty(catagory.Ratings);
+        }
+    }
+}
--- UnitTests/ModelTests/RatingClassTest.cs
+++ UnitTests/ModelTests/RatingClassTest.cs
@@ -65,9 +65,69 @@ namespace UnitTests.ModelTests
         }
         
         [Fact]
-        public void CheckGetSetNotEmptyRating()
+        public void CheckUserRating()
         {
+            //arrange
+            User user1 = new User();
+            user1.City = "Oisterwijk";
+            user1.Country = "Netherlands";
+            user1.Email = "Youdonthavetoknow@gmail.com";
+            user1.Firstname = "Duncan";
+            user1.Lastname = "Schoenmakers";
+            user1.IsAdmin = true;
+            user1.Password = "NotActuallyAPassword";
+            user1.PasswordSalt = "NotActuallyASalt";
+            user1.Phonenumber = "0651418572";
+            user1.Streetaddress = "Prinses Willem Laan";
+            user1.Username = "2now";
+            user1.Zipcode = "5068RE";
+            user1.IsDisabled = true;
+            user1.UserID = "16";
+            rating.User = user1;
 
+            User user2 = rating.User;
+            //assert
+            Assert.NotEqual("Paris", user2.City);
+            Assert.NotEqual("France", user2.Country);
+            Assert.NotEqual("SiasPLoit@gmail.com", user2.Email);
+            Assert.NotEqual("Jan", user2.Firstname);
+            Assert.NotEqual("Qu're", user2.Lastname);
+            Assert.NotEqual("NotActuallyAPasswords", user2.Password);
+            Assert.NotEqual("NotActuallyASalts", user2.PasswordSalt);
+            Assert.NotEqual("0651418554", user2.Phonenumber);
+            Assert.NotEqual("Prinses Willem Straat", user2.Streetaddress);
+            Assert.NotEqual("2nowa", user2.Username);
+            Assert.NotEqual("5068GE", user2.Zipcode);
+            Assert.NotEqual("15", user2.UserID);
+            Assert.True(user2.IsDisabled);
+        }
+        [Fact]
+        public void CheckvideoRating()
+        {
+            //arrange
+            Video vid1 = new Video();
+            vid1.UrlIdentity = "Tester123";
+            vid1.Finished = true;
+
+            rating.Video = vid1;
+
+            //assert
+            Assert.Equal("Tester123", rating.Video.UrlIdentity);
+            Assert.True(rating.Video.Finished);
+        }
+        [Fact]
+        public void CheckCategoryRating()
+        {
+            Category cat1 = new Category();
+            cat1.ParentID = 5;
+            cat1.Name = "Honda";
+            cat1.UniqueID = 10;
+
+            rating.Category = cat1;
+
+            Assert.Equal(5, rating.Category.ParentID);
+            Assert.Equal("Honda", rating.Category.Name);
+            Assert.Equal(10, rating.Category.UniqueID);
         }
     }
 }
