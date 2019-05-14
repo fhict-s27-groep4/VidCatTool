feaa3fc0ed7a45ae6a2aa9a493af7afbe94f2bfd
BlackMania <BlackMania@users.noreply.github.com>
2019-05-14 09:18:15 +0200

    Added DBContext

---
+++ Data Layer/IDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Data_Layer
+{
+    public interface IDBContext
+    {
+        DbConnection DbConnection { get; }
+        DbCommand DbCommand { get; }
+        DbDataAdapter DataAdapter { get; }
+    }
+}
---
+++ Data Layer/MySQLDBContext.cs
@@ -0,0 +1,71 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Data_Layer
+{
+    public class MySQLDBContext : IDBContext
+    {
+        private DbConnection dbConnection;
+        private DbCommand dbCommand;
+        private DbDataAdapter adapter;
+
+        private string connectionString;
+
+        public MySQLDBContext()
+        {
+            connectionString = "Server=localhost;Database=test;Uid=root;Pwd=;";
+        }
+
+
+        public DbConnection DbConnection
+        {
+            get
+            {
+                if (dbConnection == null)
+                {
+                    dbConnection = new MySqlConnection(connectionString);
+                }
+                return dbConnection;
+            }
+            set
+            {
+                dbConnection = value;
+            }
+        }
+        public DbCommand DbCommand
+        {
+            get
+            {
+                if (dbCommand == null)
+                {
+                    dbCommand = new MySqlCommand();
+                    dbCommand.Connection = DbConnection;
+                }
+                return dbCommand;
+            }
+            set
+            {
+                dbCommand = value;
+            }
+        }
+
+        public DbDataAdapter DataAdapter
+        {
+            get
+            {
+                if (adapter == null)
+                {
+                    adapter = new MySqlDataAdapter();
+                }
+                return adapter;
+            }
+            set
+            {
+                adapter = value;
+            }
+        }
+    }
+}
