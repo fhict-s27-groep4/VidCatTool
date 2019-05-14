ea2e0aa60afadb9657bdcf1bdeefc787318fa5e7
Niek Sleddens <I413747@fhict.nl>
2019-05-14 10:09:40 +0200

    json writer: removal of it's own interface

--- Model Layer/Interface/IRating.cs
+++ Model Layer/Interface/IRating.cs
@@ -4,14 +4,9 @@ using System.Text;
 
 namespace Model_Layer.Interface
 {
-    public interface IRating
+    public interface IRating : IDuncan
     {
-        string VideoIdentity { get; set; }
-        int CategoryID { get; set; }
         bool IsIABDivergent { get; set; }
-        int PleasureIndex { get; set; }
-        int ArrousalIndex { get; set; }
-        int DominanceIndex { get; set; }
         bool IsPADDivergentt { get; set; }
     }
 }
