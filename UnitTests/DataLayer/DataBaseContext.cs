9ec7d10fd131198cf92b99ee0467d7d0f55856e0
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-05-15 15:00:31 +0200

    Trying database testing, not working

---
+++ UnitTests/DataLayer/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+using Data_Layer;
+
+namespace UnitTests.DataLayer
+{
+    internal class DataBaseContext : IDBContext
+    {
+        public DbConnection DbConnection => throw new System.NotImplementedException();
+
+        public DbCommand DbCommand => throw new System.NotImplementedException();
+
+        public DbDataAdapter DataAdapter => throw new System.NotImplementedException();
+    }
+}
