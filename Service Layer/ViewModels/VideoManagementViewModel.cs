06f73fa0a282cb3c893b4e7c01db0c127839d6d4
Vincent Muijtjens <411946@student.fontys.nl>
2019-05-15 22:03:05 +0200

    added a few properties

--- Service Layer/ViewModels/VideoManagementViewModel.cs
+++ Service Layer/ViewModels/VideoManagementViewModel.cs
@@ -7,6 +7,12 @@ namespace Service_Layer.ViewModels
 {
     public class VideoManagementViewModel
     {
+        public ISearchVideo Video { get; set; }
+        public string Title { get; set; }
+        public string Thumbnail { get; set; }
+
+
+        // Deze moet uiteindelijk weg 
         public IReadOnlyCollection<ISearchVideo> Videos { get; set; }
     }
 }
