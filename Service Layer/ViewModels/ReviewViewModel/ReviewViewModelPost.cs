1eb35f6e82bead8b36d6a151a8b8a5442c9ff05e
Vincent Muijtjens <411946@student.fontys.nl>
2019-05-15 21:47:04 +0200

    Changed css and html

--- Service Layer/ViewModels/ReviewViewModel/ReviewViewModelPost.cs
+++ Service Layer/ViewModels/ReviewViewModel/ReviewViewModelPost.cs
@@ -10,16 +10,17 @@ namespace Service_Layer.ViewModels
     {
         [Required]
         public string VideoIdentity { get; set; }
+
         [Required(ErrorMessage = "Category is required")]
         public int IABID { get; set; }
 
-        [Required(ErrorMessage = "Pleasure value is required"), Range(1, 5, ErrorMessage = "Value must be between 1 and 5")]
+        [Required(ErrorMessage = "Pleasure value is required"), Range(1, 5, ErrorMessage = "Pleasure value must be between 1 and 5")]
         public int Pleasure { get; set; }
 
-        [Required(ErrorMessage = "Arrousal value is required"), Range(1, 5, ErrorMessage = "Value must be between 1 and 5")]
+        [Required(ErrorMessage = "Arrousal value is required"), Range(1, 5, ErrorMessage = "Pleasure value must be between 1 and 5")]
         public int Arrousal { get; set; }
 
-        [Required(ErrorMessage = "Dominance value is required"), Range(1, 5, ErrorMessage = "Value must be between 1 and 5")]
+        [Required(ErrorMessage = "Dominance value is required"), Range(1, 5, ErrorMessage = "Pleasure value must be between 1 and 5")]
         public int Dominance { get; set; }
     }
 }
