c48db02efe59d5d0435d450ba15dffb0cdee0424
BlackMania <BlackMania@users.noreply.github.com>
2019-05-21 10:43:38 +0200

    fixed unittest

--- UnitTests/DataLayer/DataBaseContext.cs
+++ UnitTests/DataLayer/DataBaseContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Common;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
 using Data_Layer;
 
 namespace UnitTests.DataLayer
@@ -10,5 +12,25 @@ namespace UnitTests.DataLayer
         public DbCommand DbCommand => throw new System.NotImplementedException();
 
         public DbDataAdapter DataAdapter => throw new System.NotImplementedException();
+
+        public IEnumerable<Tuple<int, string>> ExecuteNonObjectStoredProcedure(string procedurename, DbParameter[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ExecuteReturnStoredProcedure<T>(string procedurename, DbParameter[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteStoredProcedure(string procedurename, DbParameter[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> SelectQuery<T>()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
