b937d7bb59a0ae95765de63cf20cbdbb6312598c
Niek Sleddens <I413747@fhict.nl>
2019-06-01 16:15:31 +0200

    % voor martin

--- Service Layer/ViewModels/AlgoritmSettingsModel.cs
+++ Service Layer/ViewModels/AlgoritmSettingsModel.cs
@@ -6,9 +6,25 @@ namespace Service_Layer.ViewModels
 {
     public class AlgoritmSettingsModel
     {
-        public double IabToleranceTier1 { get; set; }
-        public double IabToleranceTier2 { get; set; }
-        public double BiggestPercentIAB { get; set; }
+        private double iabTolerance1;
+        private double iabTolerance2;
+        private double biggestpercentage;
+
+        public double IabToleranceTier1
+        {
+            get { return iabTolerance1 * 100; }
+            set { iabTolerance1 = value / 100; }
+        }
+        public double IabToleranceTier2
+        {
+            get { return iabTolerance2 * 100; }
+            set { iabTolerance2 = value / 100; }
+        }
+        public double BiggestPercentIAB
+        {
+            get { return biggestpercentage * 100; }
+            set { biggestpercentage = value / 100; }
+        }
         public double PadTolerance { get; set; }
         public int MaximumRatings { get; set; }
     }
