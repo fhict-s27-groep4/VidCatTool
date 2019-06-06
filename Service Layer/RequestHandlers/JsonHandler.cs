8ce50673406fedb0c4557a4d2ba44af53ab4d4aa
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-06-06 12:48:38 +0200

    added service layer to datalayer jsonHandler

---
+++ Service Layer/RequestHandlers/JsonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Data_Layer;
+using System.Collections.Generic;
+using System.Text;
+using Model_Layer.Models;
+using Data_Layer.Interface;
+
+namespace Service_Layer.RequestHandlers
+{
+    public class JsonHandler
+    {
+        private readonly IJsonAddRepository jsonrepo;
+        public JsonHandler(IEnumerable<AddJson> ids, IJsonAddRepository jsonrepo)
+        {
+            this.jsonrepo = jsonrepo ?? throw new NullReferenceException();
+        }
+        public void AddJsonFile(IEnumerable<AddJson> jsons)
+        {
+            jsonrepo.AddJsonVideos(jsons);
+        }
+    }
+}
