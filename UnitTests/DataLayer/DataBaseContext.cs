423dd21710052e69bb00868a6e129d035dfecd56
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-06-06 11:25:35 +0200

    removed useless nuget packages

--- UnitTests/DataLayer/DataBaseContext.cs
+++ UnitTests/DataLayer/DataBaseContext.cs
@@ -1,42 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using Data_Layer;
 using Model_Layer.Interface;
 
 namespace UnitTests.DataLayer
 {
-    internal class DataBaseContext : IDBContext
-    {
-        public DbConnection DbConnection => throw new System.NotImplementedException();
+    //internal class DataBaseContext : IDBContext
+    //{
+    //    public DbConnection DbConnection => throw new System.NotImplementedException();
 
-        public DbCommand DbCommand => throw new System.NotImplementedException();
+    //    public DbCommand DbCommand => throw new System.NotImplementedException();
 
-        public DbDataAdapter DataAdapter => throw new System.NotImplementedException();
+    //    public DbDataAdapter DataAdapter => throw new System.NotImplementedException();
 
-        public IEnumerable<Tuple<int, string>> ExecuteNonObjectStoredProcedure(string procedurename, DbParameter[] parameters)
-        {
-            throw new NotImplementedException();
-        }
+    //    public IEnumerable<Tuple<int, string>> ExecuteNonObjectStoredProcedure(string procedurename, DbParameter[] parameters)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public IEnumerable<IObjectPair<T1, T2>> ExecuteNonObjectStoredProcedure<T1, T2>(string procedurename, DbParameter[] parameters)
-        {
-            throw new NotImplementedException();
-        }
+    //    public IEnumerable<IObjectPair<T1, T2>> ExecuteNonObjectStoredProcedure<T1, T2>(string procedurename, DbParameter[] parameters)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public IEnumerable<T> ExecuteReturnStoredProcedure<T>(string procedurename, DbParameter[] parameters)
-        {
-            throw new NotImplementedException();
-        }
+    //    public IEnumerable<T> ExecuteReturnStoredProcedure<T>(string procedurename, DbParameter[] parameters)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public void ExecuteStoredProcedure(string procedurename, DbParameter[] parameters)
-        {
-            throw new NotImplementedException();
-        }
+    //    public void ExecuteStoredProcedure(string procedurename, DbParameter[] parameters)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public IEnumerable<T> SelectQuery<T>()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public IEnumerable<T> SelectQuery<T>()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
