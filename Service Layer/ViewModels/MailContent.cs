3e16eb9bfd34bb3152126a418beff0c0a66c3641
Niek Sleddens <I413747@fhict.nl>
2019-06-13 10:57:02 +0200

    added attributes

--- Service Layer/ViewModels/MailContent.cs
+++ Service Layer/ViewModels/MailContent.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Service_Layer.ViewModels
 {
     public class MailContent
     {
+        [Required, Display(Name = "Subject", Prompt = "Subject")]
         public string Subject { get; set; }
+        [Required, Display(Name = "Content", Prompt = "Content")]
         public string Content { get; set; }
     }
 }
