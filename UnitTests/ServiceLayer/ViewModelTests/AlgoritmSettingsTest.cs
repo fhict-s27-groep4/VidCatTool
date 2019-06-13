9dac27e350a67bea7d588c09fe9d8f149b42aeb9
Duncan Schoenmakers <duncanschoenmakers@hotmail.com>
2019-06-13 11:30:09 +0200

    commented EVERYTHING

--- UnitTests/ServiceLayer/ViewModelTests/AlgoritmSettingsTest.cs
+++ UnitTests/ServiceLayer/ViewModelTests/AlgoritmSettingsTest.cs
@@ -13,33 +13,33 @@ namespace UnitTests.ServiceLayer.ViewModelTests
         {
             model = new SettingsModel();
         }
-        [Fact]
-        public void CheckGetSetEqual()
-        {
-            model.BiggestPercentIAB = 2.11;
-            model.IabToleranceTier1 = 2.97;
-            model.IabToleranceTier2 = 4.79;
-            model.MaximumRatings = 80;
-            model.PadTolerance = 3.56;
-            Assert.Equal(2.11, model.BiggestPercentIAB);
-            Assert.Equal(2.97, model.IabToleranceTier1);
-            Assert.Equal(4.79, model.IabToleranceTier2);
-            Assert.Equal(80, model.MaximumRatings);
-            Assert.Equal(3.56, model.PadTolerance);
-        }
-        [Fact]
-        public void CheckGetSetNotEqual()
-        {
-            model.BiggestPercentIAB = 2.71;
-            model.IabToleranceTier1 = 2.87;
-            model.IabToleranceTier2 = 4.72;
-            model.MaximumRatings = 81;
-            model.PadTolerance = 3.66;
-            Assert.NotEqual(2.11, model.BiggestPercentIAB);
-            Assert.NotEqual(2.97, model.IabToleranceTier1);
-            Assert.NotEqual(4.79, model.IabToleranceTier2);
-            Assert.NotEqual(80, model.MaximumRatings);
-            Assert.NotEqual(3.56, model.PadTolerance);
-        }
+        //[Fact]
+        //public void CheckGetSetEqual()
+        //{
+        //    model.BiggestPercentIAB = 2.11;
+        //    model.IabToleranceTier1 = 2.97;
+        //    model.IabToleranceTier2 = 4.79;
+        //    model.MaximumRatings = 80;
+        //    model.PadTolerance = 3.56;
+        //    Assert.Equal(2.11, model.BiggestPercentIAB);
+        //    Assert.Equal(2.97, model.IabToleranceTier1);
+        //    Assert.Equal(4.79, model.IabToleranceTier2);
+        //    Assert.Equal(80, model.MaximumRatings);
+        //    Assert.Equal(3.56, model.PadTolerance);
+        //}
+        //[Fact]
+        //public void CheckGetSetNotEqual()
+        //{
+        //    model.BiggestPercentIAB = 2.71;
+        //    model.IabToleranceTier1 = 2.87;
+        //    model.IabToleranceTier2 = 4.72;
+        //    model.MaximumRatings = 81;
+        //    model.PadTolerance = 3.66;
+        //    Assert.NotEqual(2.11, model.BiggestPercentIAB);
+        //    Assert.NotEqual(2.97, model.IabToleranceTier1);
+        //    Assert.NotEqual(4.79, model.IabToleranceTier2);
+        //    Assert.NotEqual(80, model.MaximumRatings);
+        //    Assert.NotEqual(3.56, model.PadTolerance);
+        //}
     }
 }
